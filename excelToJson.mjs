abf4a2cdef037088b891b32eb32a2834814a1363
sfwang <fy800328@gmail.com>
2024-07-24 23:55:42 +0800

    refactor: rename variables

--- excelToJson.mjs
+++ excelToJson.mjs
@@ -5,11 +5,11 @@ const fieldsFile = fs.readFileSync('fields.json', 'utf-8')
 const myFields = JSON.parse(fieldsFile)
 
 const candidateMap = {
-  蔡英文: '金色曠野同盟',
-  韓國瑜: '蔚藍海岸陣線',
-  宋楚瑜: '鬱蔥雨林聯盟',
-  朱立倫: '蔚藍海岸陣線',
-  馬英九: '蔚藍海岸陣線',
+  蔡英文: 'savannah',
+  韓國瑜: 'coast',
+  宋楚瑜: 'rainforest',
+  朱立倫: 'coast',
+  馬英九: 'coast',
 }
 
 function removeSpace(str) {
@@ -22,6 +22,39 @@ function formatValue(value) {
   if (!str) return str
   return Number.isNaN(Number(str)) ? str : parseFloat(str)
 }
+
+const fieldMap = {
+  administrativeDivision: '行政區別',
+  village: '村里別',
+  candidateVotes: '候選人票數',
+  validVotes: '有效票數',
+  invalidVotes: '無效票數',
+  totalVotes: '投票數',
+  receivedUnvoted: '已領未投票數',
+  issuedVotes: '發出票數',
+  remainingVotes: '用餘票數',
+  eligibleVoters: '選舉人數',
+  voterTurnout: '投票率"',
+}
+
+// Sample data
+// {
+//   "administrativeDivision": "Lugu Township",
+//   "village": "Total",
+//   "candidateVotes": {
+//     "savannah": 4845,
+//     "coast": 5718,
+//     "rainforest": 308
+//   },
+//   "validVotes": 10871,
+//   "invalidVotes": 110,
+//   "totalVotes": 10981,
+//   "receivedUnvoted": 0,
+//   "issuedVotes": 10981,
+//   "remainingVotes": 4772,
+//   "eligibleVoters": 15753,
+//   "voterTurnout": 69.70999908447266
+// }
 
 export function convertExcelToJson({ path, isVillage = false, year }) {
   const workbook = XLSX.readFile(path)
@@ -50,8 +83,8 @@ export function convertExcelToJson({ path, isVillage = false, year }) {
       const cellValue = worksheet[XLSX.utils.encode_cell(cellAddress)]
 
       if (Object.keys(candidateMap).includes(fields[C])) {
-        temp['候選人票數'] ??= {}
-        temp['候選人票數'][candidateMap[fields[C]]] = formatValue(cellValue.v)
+        temp.candidateVotes ??= {}
+        temp.candidateVotes[candidateMap[fields[C]]] = formatValue(cellValue.v)
         continue
       }
 
@@ -59,13 +92,13 @@ export function convertExcelToJson({ path, isVillage = false, year }) {
     }
 
     if (isVillage) {
-      if (temp['行政區別'] !== '' && temp['村里別'] === '') {
-        currentDistrict = temp['行政區別']
-        jsonData.push({ ...temp, 村里別: '總計' })
+      if (temp.administrativeDivision !== '' && temp.village === '') {
+        currentDistrict = temp.administrativeDivision
+        jsonData.push({ ...temp, village: '總計' })
         continue
       }
 
-      temp['行政區別'] = currentDistrict
+      temp.administrativeDivision = currentDistrict
     }
 
     jsonData.push(temp)
@@ -77,7 +110,7 @@ export function storeValueToOutput(year) {
   const directoryPath = `source/${year}`
 
   const administrative_area = JSON.parse(
-    fs.readFileSync(`administrative_area/${year}.json`, 'utf-8'),
+    fs.readFileSync(`administrative_area/administrative_area.json`, 'utf-8'),
   )
 
   try {
@@ -97,28 +130,35 @@ export function storeValueToOutput(year) {
 
       if (result.length === 0) return
 
-      const cityname = filename
-        .substring(filename.indexOf('(') + 1, filename.indexOf(')'))
-        .replace(/\s/g, '')
-
       if (isVillage) {
-        for (const city in administrative_area) {
-          for (const district of administrative_area[city]) {
-            const data = result.filter((row) => row['行政區別'] === district)
+        for (const cityId in administrative_area) {
+          for (const districtId in administrative_area[cityId].districts) {
+            const data = result.filter(
+              (row) =>
+                row.administrativeDivision ===
+                administrative_area[cityId].districts[districtId].name,
+            )
             if (data.length === 0) continue
 
             const jsonContent = JSON.stringify(data, null, 2)
-            fs.writeFileSync(
-              `output/${year}/${city}-${district}.json`,
-              jsonContent,
-            )
+            fs.writeFileSync(`votes/${year}/${districtId}.json`, jsonContent)
             continue
           }
         }
       } else {
+        const cityName = filename
+          .substring(filename.indexOf('(') + 1, filename.indexOf(')'))
+          .replace(/\s/g, '')
+
+        const cityId = (
+          Object.values(administrative_area).find(
+            ({ name }) => name.slice(0, 2) === cityName.slice(0, 2),
+          ) || {}
+        ).id
+
         const jsonContent = JSON.stringify(result, null, 2)
         fs.writeFileSync(
-          `output/${year}/${cityname === '中央' ? '全國' : cityname}.json`,
+          `votes/${year}/${cityId ? cityId : 'all'}.json`,
           jsonContent,
         )
       }
